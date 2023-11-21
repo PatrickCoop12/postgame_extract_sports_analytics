7ffa179f193688cdc2c40e8a2b64cbc6e8d23202
PatrickCoop12 <123266129+PatrickCoop12@users.noreply.github.com>
2023-11-20 16:22:54 -0800

    Update app4_test.py

--- app4_test.py
+++ app4_test.py
@@ -95,9 +95,9 @@ def order_points(pts):
    # bottom-right point will have the largest sum
    rect[2] = pts[np.argmax(s)]
 
-   '''computing the difference between the points, the
-   top-right point will have the smallest difference,
-   whereas the bottom-left will have the largest difference'''
+   #computing the difference between the points, the
+   #top-right point will have the smallest difference,
+   #whereas the bottom-left will have the largest difference
    diff = np.diff(pts, axis = 1)
    rect[1] = pts[np.argmin(diff)]
    rect[3] = pts[np.argmax(diff)]
@@ -111,20 +111,20 @@ def perspective_transform(image, pts):
    rect = order_points(pts)
    (tl, tr, br, bl) = rect
 
-   '''compute the width of the new image, which will be the
-   maximum distance between bottom-right and bottom-left
-   x-coordinates or the top-right and top-left x-coordinates'''
+   #compute the width of the new image, which will be the
+   #maximum distance between bottom-right and bottom-left
+   #x-coordinates or the top-right and top-left x-coordinates
    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
    maxWidth = max(int(widthA), int(widthB))
 
-   '''compute the height of the new image, which will be the
-   maximum distance between the top-left and bottom-left y-coordinates'''
+   #compute the height of the new image, which will be the
+   #maximum distance between the top-left and bottom-left y-coordinates
    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
    maxHeight = max(int(heightA), int(heightB))
 
-   '''construct the set of destination points to obtain an overhead shot'''
+   #construct the set of destination points to obtain an overhead shot
    dst = np.array([
       [0, 0],
       [maxWidth - 1, 0],
