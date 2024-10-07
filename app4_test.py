10e58721135335c5017930c39b72f5fcb9ae5d3e
PatrickCoop12 <123266129+PatrickCoop12@users.noreply.github.com>
2024-10-07 03:52:01 -0700

    Update app4_test.py

--- app4_test.py
+++ app4_test.py
@@ -66,7 +66,7 @@ def document_to_retriever(document, chunk_size, chunk_overlap):
 # Response generation function
 def generate_response(retriever, input_text):
     memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
-    qa = ConversationalRetrievalChain.from_llm(ChatOpenAI(model_name="gpt-4-1106-preview", temperature=0,
+    qa = ConversationalRetrievalChain.from_llm(ChatOpenAI(model_name="gpt-4o", temperature=0,
                                                           openai_api_key=OPENAI_API_KEY),
                                                retriever=retriever, verbose=True, memory=st.session_state.memory)
     result = qa({"chat_history": chat_history, "question":input_text})
